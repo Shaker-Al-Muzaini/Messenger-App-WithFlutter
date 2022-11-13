d5ecd6ddd8db22418ea92d9ca3087643dc16b16c
Shaker Al Muzaini <92122042+Shaker-Al-Muzaini@users.noreply.github.com>
2022-11-13 16:45:31 +0200

    Update channels.php

--- routes/channels.php
+++ routes/channels.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Broadcast;
 /* 
 |--------------------------------------------------------------------------    
 | Broadcast Channels    
-|-------------------------------------------------------------------------- 
+|--------------------------------------------------------------------------  
 | 
 | Here you may register all of the event broadcasting channels that your
 | application supports. The given channel authorization callbacks are
