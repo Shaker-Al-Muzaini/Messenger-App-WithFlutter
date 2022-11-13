32190454a6315bfb66bbb4798166ff0999b1b3f2
Shaker Al Muzaini <92122042+Shaker-Al-Muzaini@users.noreply.github.com>
2022-11-13 17:54:22 +0200

    Update auth.php

--- config/auth.php
+++ config/auth.php
@@ -7,7 +7,7 @@ return [
     | Authentication Defaults
     |--------------------------------------------------------------------------
     |
-    | This option controls the default authentication "guard" and password
+    | This option controls the default authentication "guard" and password 
     | reset options for your application. You may change these defaults
     | as required, but they're a perfect start for most applications.
     |
