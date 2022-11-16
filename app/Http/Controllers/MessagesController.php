4eef2a89aa9bf7a2b406a2b015371f9fda301832
Shaker Al Muzaini <92122042+Shaker-Al-Muzaini@users.noreply.github.com>
2022-11-16 19:33:09 +0200

    Update MessagesController.php

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Events\MessageCreate;  
 use App\Models\Conversation;  
 use App\Models\Recipient;  
-use App\Models\User;
+use App\Models\User; 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
