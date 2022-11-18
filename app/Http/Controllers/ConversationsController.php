6818b415d9bd637b462dd51c11e6cc1c01d0049a
Shaker Al Muzaini <92122042+Shaker-Al-Muzaini@users.noreply.github.com>
2022-11-18 17:32:59 +0200

    Update ConversationsController.php

--- app/Http/Controllers/ConversationsController.php
+++ app/Http/Controllers/ConversationsController.php
@@ -2,17 +2,17 @@
 
 namespace App\Http\Controllers;
 use App\Models\Conversation;
-use Carbon\Carbon;
+use Carbon\Carbon; 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+ 
 class ConversationsController extends Controller
 {
-    public function index (){
+    public function index (){ 
         $user =Auth::user();
         return $user->conversations()->paginate();
     }
-
+ 
 
     public function show (Conversation $conversation)
     {
@@ -20,18 +20,18 @@ class ConversationsController extends Controller
         return   $conversation->load('participants');
 
 
-    }
+    } 
 
     public function addParticipant (Request $request, Conversation $conversation)
     {
         $request->validate([
             'user_id' => ['required', 'int', 'exists:users,id'],
         ]);
-
+ 
         $conversation->participants()->attach($request->post('user_id'), [
             'joined_at' => Carbon::now(),
         ]);
-        return $conversation;
+        return $conversation; 
 
     }
 
