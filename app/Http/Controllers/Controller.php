37ebf332d545abf296f393b7baed8327e96e4db6
Shaker Al Muzaini <92122042+Shaker-Al-Muzaini@users.noreply.github.com>
2022-11-18 17:30:32 +0200

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-  
+   
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;                  
 use Illuminate\Foundation\Bus\DispatchesJobs;    
 use Illuminate\Foundation\Validation\ValidatesRequests; 
