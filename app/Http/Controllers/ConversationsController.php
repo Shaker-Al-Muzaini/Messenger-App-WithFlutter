924d6d109366b9de00402a9cfbe90264658bc49c
Shaker Al Muzaini <92122042+Shaker-Al-Muzaini@users.noreply.github.com>
2022-11-21 13:07:07 +0200

    Update ConversationsController.php

--- app/Http/Controllers/ConversationsController.php
+++ app/Http/Controllers/ConversationsController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\Conversation;
 use Carbon\Carbon;
-use Illuminate\Http\Request;
+use Illuminate\Http\Request; 
 use Illuminate\Support\Facades\Auth;
 
 class ConversationsController extends Controller
