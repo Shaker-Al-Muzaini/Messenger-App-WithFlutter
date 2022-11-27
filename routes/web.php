a91f85f8318d6f70236f6f5ca938e376282ceebc
Shaker Al Muzaini <92122042+Shaker-Al-Muzaini@users.noreply.github.com>
2022-11-27 19:47:23 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\MessageController;
 use App\Http\Controllers\ProfileController; 
 use Illuminate\Support\Facades\Route;      
  
-/*
+/* 
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
