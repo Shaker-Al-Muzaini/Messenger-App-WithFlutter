179647afa0bc014c69a3ece210271fb0c9117ed5
Shaker Al Muzaini <92122042+Shaker-Al-Muzaini@users.noreply.github.com>
2022-11-28 15:52:52 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php', 
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
     ],
