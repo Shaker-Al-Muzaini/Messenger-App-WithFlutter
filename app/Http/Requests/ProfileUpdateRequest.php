cb08ab98eff6d40c912cfc12c9e65d3221eefaaf
Shaker Al Muzaini <92122042+Shaker-Al-Muzaini@users.noreply.github.com>
2022-12-01 16:25:37 +0200

    Update ProfileUpdateRequest.php

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use App\Models\User;
 use Illuminate\Foundation\Http\FormRequest;   
 use Illuminate\Validation\Rule;
-
+ 
 class ProfileUpdateRequest extends FormRequest
 {
     /**
