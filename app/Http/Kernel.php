5be65fbdb2ca20ae902edf989e78d9a6b2caaf50
Shaker Al Muzaini <92122042+Shaker-Al-Muzaini@users.noreply.github.com>
2022-12-06 17:18:13 +0200

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -5,7 +5,7 @@ namespace App\Http;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;     
   
 class Kernel extends HttpKernel 
-{
+{ 
     /**
      * The application's global HTTP middleware stack.
      *
